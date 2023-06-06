538405a416b1d6c9745ec08a713f95b40a77eeda
Philippe Boyd <philippe.boyd@maxa.ai>
2023-06-06 18:15:06 -0400

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,85 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Maxa AI\'s Public Documentation',
+  tagline: 'The fastest way to make enterprise data friendly to business teams and code',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://docs.maxa.ai',
+  baseUrl: '/',
+
+  organizationName: 'maxa-ai',
+  projectName: 'maxa-ai.github.io',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Maxa AI',
+        logo: {
+          alt: 'Maxa AI Logo',
+          src: 'img/logo.svg',
+        },
+        items: [],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Website',
+                href: 'https://www.maxa.ai',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/maxa-ai/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Maxa AI, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
